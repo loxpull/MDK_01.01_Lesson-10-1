6f236037ed861e6e9aaa72efed580d66beaf5336
loxpull <165044612+loxpull@users.noreply.github.com>
2025-01-27 23:17:52 +0300

    Add files via upload

---
+++ MDK_01.01_Lesson-10-1/MDK_01.01_Lesson-10-2/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Translator translator = new EnglishTranslator();
+        translator.Translate();
+        translator.GetLanguage();
+
+}
+    public abstract class Translator
+    {
+        public abstract void Translate();
+
+        public abstract void GetLanguage();
+    }
+
+    public class EnglishTranslator : Translator
+    {
+        public override void Translate()
+        {
+            Console.WriteLine("Я переводчик с английского");
+        }
+
+        public override void GetLanguage()
+        {
+            Console.WriteLine("Это английский язык");
+        }
+    }
+}
+
+
