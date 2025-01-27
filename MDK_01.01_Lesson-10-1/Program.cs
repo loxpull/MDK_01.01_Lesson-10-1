d2a85e916ef85a72b4d677828fb39b06b36f815a
loxpull <165044612+loxpull@users.noreply.github.com>
2025-01-27 16:59:36 +0300

    Add files via upload

---
+++ MDK_01.01_Lesson-10-1/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Screen screen = new Screen();
+
+        screen.OnSelect();
+
+        screen.Refresh();
+    }
+
+
+    public interface ISelectable
+    {
+        void OnSelect();
+    }
+
+    public interface IUpdatable
+    {
+        void Refresh();
+    }
+
+    public class Screen : ISelectable, IUpdatable
+    {
+        public void OnSelect()
+        {
+            Console.WriteLine("Экран выбран.");
+        }
+
+        public void Refresh()
+        {
+            Console.WriteLine("Экран обновлён.");
+        }
+    }
+}
